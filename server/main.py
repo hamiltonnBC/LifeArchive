e80224e937434c5ff499ff9ea85e1e69f08ceb9c
Nicholas Hamilton <hamiltonn@berea.edu>
2024-12-30 13:06:09 -0500

    started backend setup

--- server/main.py
+++ server/main.py
@@ -1,2 +1,20 @@
 
+from flask import Flask, request, jsonify
 
+
+app = Flask(__name__)
+
+@app.route('/api/users', methods=['GET'])
+def users():
+    return jsonify(
+
+        {'users':
+             ['Alice',
+              'Bob',
+              'Charlie']
+        }
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
